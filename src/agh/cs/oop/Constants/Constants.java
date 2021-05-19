62e5bef135a9a410508a90762351a17a6a044755
kenaj99 <banot.jan@gmail.com>
2021-05-19 20:39:34 +0200

    ADD: Draft constants changes

---
+++ src/agh/cs/oop/Constants/Constants.java
@@ -0,0 +1,25 @@
+package agh.cs.oop.Constants;
+
+public class Constants {
+    public static final String TITLE = "Evolution Simulator";
+    public static final int BOARD_WIDTH = 1000;
+    public static final int BOARD_HEIGHT = 1000;
+
+    //Values coming from JSON input file
+    public static int NUMBER_OF_SQUARES_X;
+    public static int NUMBER_OF_SQUARES_Y;
+    public static int NUMBER_OF_MAPS_TO_GENERATE;
+    public static int GAME_SPEED; //constant defining map time refresh frequency
+    public static int START_ENERGY;
+    public static int NUMBER_OF_ANIMALS;
+    public static int MOVE_ENERGY;
+    public static int PLANT_ENERGY;
+    public static int JUNGLE_DESERT_RATIO;
+
+    //Dinosaur level based on it's energy, Level 1 - lowest, Level 4 - highest
+    public static int LEVEL1; //White Dino
+    public static int LEVEL2; //Blue Dino
+    public static int LEVEL3; //Red Dino
+    public static int LEVEL4; //Pink Dino
+
+}
